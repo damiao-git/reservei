56bf399c2c9371c456b3996ac02d60a93a138d9c
Marco Antonio <marco.nunes@hfa.mil.br>
2024-10-11 07:38:39 -0300

    UserType (2/2)

--- src/main/java/br/com/reservei/controller/UserTypeController.java
+++ src/main/java/br/com/reservei/controller/UserTypeController.java
@@ -3,14 +3,15 @@ package br.com.reservei.controller;
 import br.com.reservei.dto.UserTypeRecordDto;
 import br.com.reservei.entity.UserType;
 import br.com.reservei.repository.UserTypeRepository;
-import org.apache.catalina.User;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -24,7 +25,6 @@ public class UserTypeController {
     public ResponseEntity<UserType> saveUserType(@RequestBody UserTypeRecordDto userTypeRecordDto){
 
         var userTypeNew = new UserType();
-
         BeanUtils.copyProperties(userTypeRecordDto, userTypeNew);
 
         return ResponseEntity.status(HttpStatus.OK).body(userTypeRepository.save(userTypeNew));
@@ -34,4 +34,48 @@ public class UserTypeController {
     public ResponseEntity<List<UserType>> getAllUserType(){
         return ResponseEntity.status(HttpStatus.OK).body(userTypeRepository.findAll());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOne(@PathVariable(value = "id") Integer id){
+
+        Optional<UserType> item = userTypeRepository.findById(id);
+
+        if(item.isEmpty()){
+
+            Map<String, String> response = new HashMap<>();
+            response.put("mensagem", "Dado não encontrado");
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(userTypeRepository.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserType> updateUserType(@PathVariable(value = "id") Integer id, @RequestBody UserTypeRecordDto userTypeRecordDto){
+
+        var UserTypeNew = new UserType();
+
+        BeanUtils.copyProperties(userTypeRecordDto, UserTypeNew);
+
+        return ResponseEntity.status(HttpStatus.OK).body(UserTypeNew);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteUserType(@PathVariable(value = "id") Integer id){
+
+        Optional<UserType> item = userTypeRepository.findById(id);
+
+        if(item.isEmpty()){
+            Map<String,  String> response = new HashMap<>();
+            response.put("mensagem", "Dado não encontrado");
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+        userTypeRepository.delete(item.get());
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
 }
